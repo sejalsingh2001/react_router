c93cc8acb58d9a5b672ec8dca435ad25c341ed6d
Sejal Singh <sejal@geekyants.com>
2023-04-20 17:27:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomeComponent from "./pages/HomeComponent";
+import AccountComponent from "./pages/AccountComponent";
+import CardsComponent from "./pages/CardsComponent";
+import useAuth from "./useAuth";
+import PrivateRoute from "./PrivateRoute";
 
-function App() {
+export default function App() {
+  const [isAuth, login, logout] = useAuth(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Protected Routes Tutorial</h2>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/accounts">Accounts (Protected)</Link>
+          </li>
+          <li>
+            <Link to="/cards">Cards (UnProtected)</Link>
+          </li>
+        </ul>
+        {isAuth ? (
+          <>
+            <div className="ui message brown">You are logged in..</div>
+            <button className="ui button blue" onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="ui message violet">You are logged out..</div>
+            <button className="ui button blue" onClick={login}>
+              Login
+            </button>
+          </>
+        )}
+        <Routes>
+          <Route path="/" exact element={<HomeComponent />} />
+          <Route path="/cards" element={<CardsComponent />} />
+          {/* <ProtectedRoute path="/accounts" component={AccountComponent} auth={ isAuth}/> */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/accounts" element={<AccountComponent />} exact />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
